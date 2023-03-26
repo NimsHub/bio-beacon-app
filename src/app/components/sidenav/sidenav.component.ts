2a39e050f8db3ca230b07669cd22a7829c93919f
Nirmala Madhusankha <nirmalasabaragamuwa@gmail.com>
2023-03-26 22:12:34 +0530

    created custom css and added alternative logo

---
+++ src/app/components/sidenav/sidenav.component.ts
@@ -0,0 +1,18 @@
+import {Component, ElementRef, HostListener} from '@angular/core';
+import {FormBuilder} from "@angular/forms";
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.css']
+})
+export class SidenavComponent {
+  options = this._formBuilder.group({
+    bottom: 0,
+    fixed: false,
+    top: 0,
+  });
+
+  constructor(private _formBuilder: FormBuilder,private elementRef: ElementRef) {}
+
+}
