fb4ac436c5014128acd6545dcc440bb39c73231a
P.M.Amarasingha <85576788+PoornaManujaya@users.noreply.github.com>
2023-03-27 12:43:22 +0530

    Adjust icons

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {faYoutube} from '@fortawesome/free-brands-svg-icons'
 
 @Component({
   selector: 'app-home-page',
@@ -6,4 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+  faYoutube = faYoutube
 }
