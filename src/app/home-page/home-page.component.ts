68b11df381ee007bc7909eeadcc27420f1d02759
P.M.Amarasingha <85576788+PoornaManujaya@users.noreply.github.com>
2023-03-27 19:30:11 +0530

    Add Sports Icons

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-import {faYoutube} from '@fortawesome/free-brands-svg-icons'
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faPersonBiking } from '@fortawesome/free-solid-svg-icons';
+import { faPersonSkiing } from '@fortawesome/free-solid-svg-icons';
+import { faPersonRunning } from '@fortawesome/free-solid-svg-icons';
+import { faPersonSwimming } from '@fortawesome/free-solid-svg-icons';
+import { faPersonHiking } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent {
-  faYoutube = faYoutube
+  faYoutube = faYoutube;
+  faPersonBiking = faPersonBiking;
+  faPersonSkiing = faPersonSkiing;
+  faPersonRunning = faPersonRunning;
+  faPersonSwimming = faPersonSwimming;
+  faPersonHiking = faPersonHiking;
 }
