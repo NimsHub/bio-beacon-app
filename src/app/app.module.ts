bbe41a57df94ffa76c120e182fe6408fb493d57a
P.M.Amarasingha <85576788+PoornaManujaya@users.noreply.github.com>
2023-04-05 13:24:02 +0530

    Finish form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {LoginComponent} from './components/login/login.component';
-import {MaterialModule} from "./modules/material/material.module";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {RegistrationComponent} from './components/registration/registration.component';
-import {MatTabsModule} from "@angular/material/tabs";
-import {MatButtonToggleModule} from "@angular/material/button-toggle";
-import {LandingComponent} from './views/landing/landing.component';
-import {TableRowComponent} from './components/table-row/table-row.component';
-import {SessionTableComponent} from './components/session-table/session-table.component';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+import { MaterialModule } from './modules/material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { LandingComponent } from './views/landing/landing.component';
+import { TableRowComponent } from './components/table-row/table-row.component';
+import { SessionTableComponent } from './components/session-table/session-table.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatCheckboxModule} from "@angular/material/checkbox";
-import {MatToolbarModule} from "@angular/material/toolbar";
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ContatUsComponent } from './contat-us/contat-us.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import {MatToolbarModule} from "@angular/material/toolbar";
     LandingComponent,
     TableRowComponent,
     SessionTableComponent,
-    SidenavComponent
+    SidenavComponent,
+    ContatUsComponent,
   ],
   imports: [
     BrowserModule,
@@ -37,10 +39,9 @@ import {MatToolbarModule} from "@angular/material/toolbar";
     MatButtonToggleModule,
     MatSidenavModule,
     MatCheckboxModule,
-    MatToolbarModule
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
---
+++ src/app/contat-us/contat-us.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contat-us',
+  templateUrl: './contat-us.component.html',
+  styleUrls: ['./contat-us.component.css']
+})
+export class ContatUsComponent {
+
+}
