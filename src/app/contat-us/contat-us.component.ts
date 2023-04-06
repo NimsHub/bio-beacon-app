a947637bd17b69a8066cea45f269c0f4dba35fbb
P.M.Amarasingha <85576788+PoornaManujaya@users.noreply.github.com>
2023-04-06 12:21:20 +0530

    Finish input forms with validations and error messages

--- src/app/contat-us/contat-us.component.ts
+++ src/app/contat-us/contat-us.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contat-us',
   templateUrl: './contat-us.component.html',
-  styleUrls: ['./contat-us.component.css']
+  styleUrls: ['./contat-us.component.css'],
 })
 export class ContatUsComponent {
+  @Output() toggleEvent = new EventEmitter<boolean>();
+  userEmail = new FormControl('', [Validators.required, Validators.email]);
+  userName = new FormControl('', Validators.required);
+  message = new FormControl('', Validators.required);
+  mobile = new FormControl('', Validators.required);
+  role = new FormControl<boolean>(true);
 
+  toggle() {
+    this.toggleEvent.emit();
+  }
 }
