2bffcfc8322bb5deb1fbb92e2dc8e45540c4f6e4
nirmala <nirmalasabaragamuwa@gmail.com>
2023-05-27 21:50:46 +0530

    added session table component

--- src/app/components/sessions/session-table/session-table.component.ts
+++ src/app/components/sessions/session-table/session-table.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {Session} from "../../models/Session";
-import {SessionService} from "../../services/session.service";
+import {Session} from "../../../models/Session";
+import {SessionService} from "../../../services/session.service";
 
 @Component({
   selector: 'app-session-table',
