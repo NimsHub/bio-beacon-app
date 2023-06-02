1b08a0e90b33bb8b0a1ff7cb7b25deeadc12a69d
nirmala <nirmalasabaragamuwa@gmail.com>
2023-06-02 21:11:59 +0530

    added models for Device, CoachDetails, AthleteDetails

---
+++ src/app/models/CoachDetails.ts
@@ -0,0 +1,10 @@
+export interface CoachDetails {
+  id: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  gender: string;
+  dateOfBirth: string;
+  mobile: string;
+  address: string;
+}
