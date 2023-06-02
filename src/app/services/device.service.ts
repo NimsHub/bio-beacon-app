9ef9d45e58fb9240fd8c0e3dedca85b69b4f6ae8
nirmala <nirmalasabaragamuwa@gmail.com>
2023-06-02 21:09:38 +0530

    added device service

---
+++ src/app/services/device.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {Session} from "../models/Session";
+import {DEVICES} from "../models/mock";
+import {Device} from "../models/Device";
+import {CoachDetails} from "../models/CoachDetails";
+import {environment} from "../environments/environment.prod";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeviceService {
+
+  constructor(private http:HttpClient) {
+  }
+
+  getDevices(): Observable<Device[]> {
+    return this.http.get<Device[]>(environment.baseURL + '/api/v1/devices')
+  }
+}
