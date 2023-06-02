d2d75e8984af0a44ecea06ca44de9927a3d21ef2
nirmala <nirmalasabaragamuwa@gmail.com>
2023-06-02 21:12:38 +0530

    added environment.prod.ts

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  DefaultLanguage: "en",
+  production: false,
+  development: true,
+  environmentName:"PROD",
+  baseURL:"http://biobeaconapi-env.eba-xficc75t.us-east-1.elasticbeanstalk.com"
+};
