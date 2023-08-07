dc1eebe472ec4376cabbd4e06a8b5c8736827f00
nirmala <nirmalasabaragamuwa@gmail.com>
2023-08-07 13:36:00 +0530

    :rocket: update workflow

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   production: false,
   development: true,
   environmentName:"PROD",
-  baseURL:"http://biobeaconapi-env.eba-xficc75t.us-east-1.elasticbeanstalk.com"
+  baseURL:"api:5000"
 };
